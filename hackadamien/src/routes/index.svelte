11d24b42821de4a6ea57b2f776432fa901c8a003
10dine <ishtyaqkhan1@gmail.com>
2024-02-24 05:09:14 -0500

    Add new Svelte routes

---
+++ hackadamien/src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+    let name = 'world';
+  </script>
+  <a href="/contact">Go to contact page</a>
+  <a href="/winner">Go to winner page</a>
+  <h1>Hello {name}!</h1>
+  <p>Welcome to the home page.</p>
