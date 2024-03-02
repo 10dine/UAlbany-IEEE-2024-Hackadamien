05e9150e91b9513837a9fbb2341bcccd21145640
10dine <ishtyaqkhan1@gmail.com>
2024-03-02 17:44:25 -0500

    Update Dockerfile and Svelte config

--- hackadamien/src/routes/+page.svelte
+++ hackadamien/src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
-</script>
-  <a href="/contact">Go to contact page</a>
-  <a href="/winners">Go to winner page</a>
-  <h1>Under construction!</h1>
 
+</script>
+
+<nav>
+    <ul>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="https://discord.com">Discord</a></li>
+        <li><a href="/winners">Winners</a></li>
+    </ul>
+</nav>
+
+<h1>Under construction!</h1>
