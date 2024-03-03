995f712bf81d104131c08234cf356ea6eae4c5f1
Joshua Angulo <jangulo@albany.edu>
2024-03-02 21:11:41 -0500

    Cleanup

--- hackadamien/src/routes/winners/+page.svelte
+++
@@ -1,8 +0,0 @@
-<main>
-    <header>
-        <a href="/">Homepage</a>
-        <a href="/contact">Contact Page</a>
-    </header>
-    <h1>Welcome to the Winner's page</h1>
-    <p>The winners of each category will be posted shortly after the Hack-A-Damien is over</p>
-</main>
