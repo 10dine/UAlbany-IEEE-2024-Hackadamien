261e91ecb834e1b89ad37a7a40acfe8d0852f55b
Rueben Engelhart <engelhartrueben@gmail.com>
2025-02-19 14:20:37 -0500

    format + updatet to 2025!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 	import { onMount } from "svelte";
 
-    let predate = "February 21, 2025";
-    let date = "February 22-23, 2025";
+    let predate = "February 21, 2025 @ 5:30PM";
+    let date = "February 22-23, 2025 @ 9:00AM";
     let location = "ETEC Building, University at Albany";
     // let targetDate = new Date("April 13, 2024 09:00:00").getTime();
     let targetDate = new Date("February 22, 2025 09:00:00").getTime();
@@ -142,7 +142,7 @@
         transition: 0.3s ease-in-out;
     }
 
-    .card.dark-mode{
+    .card.dark-mode {
         height: 100%;
         width: 100%;
         position: relative;
@@ -209,6 +209,36 @@
         font-size: 12px;
     }
 
+    .sponsors {
+        height: 100%;
+        width: 100%;
+        position: relative;
+        transform-style: preserve-3d;
+        border-radius: 1rem;
+        box-shadow: 0 0 5px 2px #27228060;
+        background-color: rgb(241, 240, 240, 1);
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        transition: 0.3s ease-in-out;
+    }
+
+    .sponsors.dark-mode { 
+        height: 100%;
+        width: 100%;
+        position: relative;
+        transform-style: preserve-3d;
+        border-radius: 1rem;
+        box-shadow: 0px 0px 15px 8px #27228060;
+        background-color: rgba(241, 240, 240, 0);
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        transition: 0.3s ease-in-out;
+    }
+
     .main-container {
         display: flex;
         height: auto;
@@ -221,10 +251,7 @@
 
     .left-container,
     .right-container {
-        margin: 0 auto;
-        width: 30%;
-    }
-
+        margin: 0 auto; width: 30%; }
     .middle-container {
         margin: 0 10px;
     }
@@ -256,6 +283,7 @@
     .desc {
         color: #807db2;
         line-height: 120%;
+	max-width: 800px;
     }
 
     .sponsor-img {
@@ -479,7 +507,7 @@
                     <div class="knob"></div>
                 </div>
             </label>
-            <span class="registerheader"><a href="https://forms.gle/m5Vijy61EEJ6VF7N6">Register</a></span>
+            <span class="registerheader"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfXVyzioJokVxDQVztqTmcR9-MnhPdx_MiDuDVqR1tuQqF2tg/alreadyresponded">Register</a></span>
         </div>
     </header>
     <div>
@@ -538,10 +566,9 @@
     <div class="container">
         <div class="card" class:dark-mode={toggleState} id="UPDATES">
             <h2>Hack-A-Damien</h2>
-            <p class="desc">PLACEHOLDER - PUT WHAT IS GOING ON HERE PLEASE</p> 
-	    <p class="desc">PLACEHOLDER - something something specatators come join. See our sponsor tables (RIP FBI) etc etc</p>
-                
-
+            <p class="desc">This year's them is all about changing the way we learn. Whether you're into coding, building, desiging, or just want to try something new, this is your chance to create something amazing, form educational games to interactive learning tools. Open to CS, ECE, and all majors, no experience needed!</p> 
+	    <p class="desc">Last year's hackathon brought 120+ students together to build aming games - this year, we're expanding the theme to education and learning. Expect crazy prizes, great talks, and fun weekend of creating with friends.</p>
+	    <p class="desc"><b>Food all day!</b></p>
             <p>RULES ARE SUBJECT TO CHANGE</p>
             <p></p>
         </div>
@@ -556,97 +583,40 @@
             </div>
         </div>
         <div class="middle-container-dif">
-            <img class="tri-image-main" src="/hackadamien-logo.png" alt="">
+            <img class="tri-image-main" src="/2025_hackadamien_logo.png" alt="">
         </div>
         <div class="right-container">
             <div class="card" class:dark-mode={toggleState} id="RightSide">
                 <img class="tri-image" src="/theme_black.png" alt="">
                 <h2>Theme</h2>
-                <p class="infos">Same Level, different challenges</p>
-                <p class="infos">Ex. BTD, Tetris, Chrome Dino Game</p>
+                <p class="infos">Reinvent The Way You Learn</p>
+                <p class="infos">Something like:<br/> 
+				- Flash Card App<br/> 
+				- Educational Game<br/>
+				- Interactive Learning Tool
+		</p>
                 <p></p>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="card" class:dark-mode={toggleState}>
-            <img class="tri-image" id="registration-image" src="/register.png" alt="">
+            <a href="https://forms.gle/X8FpdDTWQmdrgn6EA"><img class="tri-image" id="registration-image" src="/register.png" alt="scroll"/></a>
             <h2>Registration Process</h2>
-            <p class="infos">If you wish to participate, please fill out this form <a href="https://forms.gle/m5Vijy61EEJ6VF7N6">here</a></p>
+            <p class="infos">If you wish to participate, please fill out this form <a href="https://forms.gle/X8FpdDTWQmdrgn6EA"><b>here</b></a></p>
             <p></p>
         </div>
     </div>
     <div class="container">
-        <div class="card" class:dark-mode={toggleState} id="Schedule">
-            <h2>Schedule</h2>
-            <h3>(Subject to Change)</h3>
-            <h4>APRIL 12th</h4>
-            <p class="infos">Arrive for Pre-Hackathon Program - 3:30PM</p>
-            <p class="infos">Grimbar Unity Mobile - 4:15-5PM</p>
-            <p class="infos">Resouce Workshop (B010) - 5-5:30PM</p>
-            <p class="infos">Team Building Workshop (B010) - 5:35-6:20PM</p>
-            <p class="infos">Dinner (Cafe area) - 5:45-6PM</p>
-            <p class="infos">C++ Web Assembly & Raylib - 6:30-7:20PM</p>
-            <p class="infos">Unity Workshop - 7:20-8:15PM</p>
-
-            <h4>APRIL 13th</h4>
-            <p class="infos">Opening doors - 8:30AM</p>
-            <p class="infos">~Breakfast available~</p>
-            <p class="infos">Check-in - 8:30-10:45AM</p>
-            <p class="infos">Vendor Fair (Atrium) - 9AM-12PM</p>
-            <p class="infos">Workshops (Training room)</p>
-            <lu>
-                <li class="infos2">IBM - 10:15-10:55AM</li>
-                <li class="infos2">Velan Studios Q&A - 11:30AM-12PM</li>
-                <!---<li class="infos2">Dean's Son-in-law | Nick's Workshop</li>-->
-            </lu>
-            <p class="infos">Opening Ceremony (Training Room) - 11-11:30AM</p>
-            <p class="infos">Brainstorming sessions/free time (Atrium) - 11:30AM-12PM</p>
-            <p class="infos">Competition begins (B010) - 12:15PM</p>
-            <p class="infos">~Lunch (Atrium) - 1PM~</p>
-            <p class="infos">Karaoke - 6PM</p>
-            <p class="infos">~Dinner (Cafe area) - 7PM~</p>
-            <p class="infos">Jeopardy - 8PM</p>
-            <p class="infos">Worst Code Workshop - 9PM</p>
-            <p class="infos">~ALL PARTICIPANTS WILL BE MOVED DOWNSTAIRS~</p>
-            <p class="infos">~Midnight snacks - 12AM~</p>
-
-            <h4>APRIL 14th</h4>
-            <p class="infos">~Breakfast - 7AM~</p>
-            <p class="infos">Submissions open - 9AM</p>
-            <p class="infos">Completition Officials Ends - 12:15PM</p>
-            <p class="infos">Judges review submissions - 1-3PM</p>
-            <p class="infos">Closing Ceremony/Awards - 3-3:30PM</p>
-            <p class="infos"></p>
-        </div>
-    </div>
-    <div class="container">
-        <div class="card" class:dark-mode={toggleState} id="Sponsors">
+        <div class="sponsors" class:dark-mode={toggleState} id="Sponsors">
             <h2>Sponsors</h2>
             <div class="logos">
                 <img class="sponsor-img" src="/cnse.png" alt="College of Nanotechnology, Science, and Engineering (CNSE)">
+		<img class="sponsor-img" src="/desmos_2.png" alt="Desmos">
+		<img class="sponsor-img" src="/Velan-Studios-logo.png" alt="Velan Studios">
             </div>
         </div>
     </div>
-    <!--
-    <div class ="main-container">
-        <div class="left-container">
-            <div class="card">
-                <p>College of Nanotechnology, Science, and Engineering (CNSE)</p>
-            </div>
-        </div>
-        <div class="middle-container">
-            <div class="card">
-                <p>International Business Machines Corporation (IBM)</p>
-            </div>
-        </div>
-        <div class="right-container">
-            <div class="card">
-                <p>Grimbar Interactive</p>
-            </div>
-        </div>
-    </div>
-    -->
     <div class="container">
     </div>
 </main>
